23194bae853d2b0cd7cbda25c065ccafbbb34fca
EscoAl516 <escobaralexander2004@gmail.com>
2025-04-29 16:44:44 -0400

    Database connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './Pages/MainPage';
+import GuestPage from './Pages/GuestPage';
 import StaffLogin from './Pages/StaffLogin';
 import CustomerLogin from './Pages/CustomerLogin';
 import HomePage from './Pages/HomePage';
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/customer-login" element={<CustomerLogin />} />
           <Route path="/staff-login" element={<StaffLogin />} />
+          <Route path="/guest-page" element={<GuestPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/search-flights" element={<SearchFlights />} />
           <Route path="/submit-ratings" element={<Ratings />} />
